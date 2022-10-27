52bb6f3d816fdda688f3ad59443089837970853f
Thiccarus-13 <113143044+Thiccarus-13@users.noreply.github.com>
2022-10-27 15:37:47 -0600

    quick changes

--- DungeonCrawler/src/dungeonLayout.java
+++ DungeonCrawler/src/dungeonLayout.java
@@ -1,4 +1,7 @@
+import java.util.*;
 public class dungeonLayout {
     // Dungeon is a Binary Tree, where nodes have pointer to siblings as well as parent nodes.
+    ArrayList<roomLayout> roomList = new ArrayList<>();
+
 
 }
